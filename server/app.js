3b8d42663f465e39e8179fe16b8eeee7ceb89944
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2020-03-21 23:09:51 +0100

    Cleanup

--- server/app.js
+++ server/app.js
@@ -3,21 +3,14 @@ var express = require('express');
 var path = require('path');
 var chalk = require('chalk')
 var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise
-
 const logger = require('./modules/logger/logger');
-
-
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
