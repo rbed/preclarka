537d0084a061be95cf6e54ca8dddce29cf90236e
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2020-03-29 18:43:32 +0200

    FIXME : HTTP.STATUS_CODES
    fix status 500 => internal server error | 200 = OK

--- server/routes/addresses.js
+++ server/routes/addresses.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+/**
+* obsługa błędów HTTP zewnetrzna biblioteką
+*  -- pakiet opcjonalny ale moze będzie Ci łatwiej pracowac
+*  -- dokumentacja biblioteki
+*    -- https://www.npmjs.com/package/http-status-codes
+*/
+//var HttpStatus = require('http-status-codes');
+
 const mongoose = require("mongoose");
 const Addresses = mongoose.model("Addresses");
 
@@ -8,8 +16,11 @@ const Addresses = mongoose.model("Addresses");
 router.get("/", async function(req, res, next) {
   try {
     var result = await Addresses.find().exec();
+    // FIXME: res.status(200), 500 => InternalServerError
+    // return res.status(HttpStatus.OK ).json({ info: result });
     return res.status(500).json({ info: result });
   } catch (err) {
+    // return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ info: result });
     return res.status(500).json({ info: err });
   }
 
@@ -34,6 +45,8 @@ router.get("/:id", async function(req, res, next) {
   try {
     var result = await Addresses.find({id : id}).exec();
     console.log(result);
+    // FIXME: res.status(200), 500 => InternalServerError
+    // return res.status(HttpStatus.OK ).json({ info: result });
     return res.status(500).json({ info: result });
   } catch (err) {
     return res.status(500).json({ info: err });
@@ -52,7 +65,7 @@ router.post("/", async function(req, res, next) {
     var Address = new Addresses(address);
     console.log(Address);
     var result = await Address.save();
-    console.log(res.status(200).json(result));
+    //console.log(res.status(200).json(result));
     return res.status(200).json(result);
   } catch (err) {
     res.status(400).json(err);
@@ -65,13 +78,14 @@ router.put("/", async function(req, res, next) {
   // console.log(req);
   var address = req.body.address; // <<<<<<<<<<< params bo odbieram dane
   if (!address) {
-    return res.status(402).send("brak danych");
+    //res.status(HttpStatus.BAD_REQUEST).send("brak danych");
+    return res.status(400).send("brak danych");
   }
   try {
     var result = await Addresses.findByIdAndUpdate(address._id, address, doc => {
       return doc;
     });
-    console.log(res.status(200).json(result));
+    //console.log(res.status(200).json(result));
     return res.status(200).json(result);
     // jak chcesz aktualizować musisz przerobić na callback
   } catch (err) {
@@ -86,11 +100,11 @@ router.delete("/:id", async function(req, res, next) {
   // console.log(req);
   var id = req.params.id; // <<<<<<<<<<< params bo odbieram dane
   if (!id) {
-    return res.status(402).send("brak id");
+    return res.status(400).send("brak id");
   }
   try {
     var result = await Addresses.deleteOne({ _id: id }).exec();
-    console.log(res.status(200).json(result));
+    //console.log(res.status(200).json(result));
     return res.status(200).json(result);
   } catch (err) {
     res.status(400).json(err);
