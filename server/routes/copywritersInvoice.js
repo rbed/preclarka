2e96eb05ae09548e9c01bcaa97a19d224c1da8b7
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2020-03-29 18:59:47 +0200

    FIXME : HTTP.STATUS_CODES + negatywne scenariusze
    //zakladamy ze ID zawsze bedzie?
    if (id) {
     try{
        //logika 
     }catch{
        // obsluga bledu
     }
    }
    
    //obsluga negatywnego zapytania

--- server/routes/copywritersInvoice.js
+++ server/routes/copywritersInvoice.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var router = express.Router();
 
+/**
+* obsługa błędów HTTP zewnetrzna biblioteką
+*  -- pakiet opcjonalny ale moze będzie Ci łatwiej pracowac
+*  -- dokumentacja biblioteki
+*    -- https://www.npmjs.com/package/http-status-codes
+*/
+//var HttpStatus = require('http-status-codes');
+
 const mongoose = require("mongoose");
 const CopywritersInvoice = mongoose.model("CopywritersInvoice");
 
@@ -18,15 +26,21 @@ router.get("/", async function(req, res, next) {
 
 // Get single copywriter - dziala
 router.get("/:id", async function(req, res, next) {
+ 
   var id = req.params.id;
+  
+  //rozpatrujesz tylko pozytywny scenariusz zapytania
   if (id) {
-  try {
-    var result = await CopywritersInvoice.find({_id : id}).exec();
-    console.log(result);
-    return res.status(200).json({ info: result });
-  } catch (err) {
-    return res.status(500).json({ info: err });
-  }}
+    try {
+      var result = await CopywritersInvoice.find({_id : id}).exec();
+      console.log(result);
+      return res.status(200).json({ info: result });
+    } catch (err) {
+      return res.status(500).json({ info: err });
+    }
+  }
+  // if not to co?
+  //return res.status(500).send('cos poszło nie tak xD')
 
 });
 
@@ -34,10 +48,13 @@ router.get("/:id", async function(req, res, next) {
 // Dodawanie copywriterow - dziala
 router.post("/", async function(req, res, next) {
   var copywriter = req.body.copywriter;
+  
   console.log(copywriter);
+ 
   if (!copywriter) {
-    return res.status(402).send("cokolwiek");
+    return res.status(400).send("cokolwiek");
   }
+  
   try {
     var copywriter = new CopywritersInvoice(copywriter);
     console.log(copywriter);
@@ -47,6 +64,7 @@ router.post("/", async function(req, res, next) {
   } catch (err) {
     res.status(400).json(err);
   }
+  
 });
 
 
@@ -54,9 +72,11 @@ router.post("/", async function(req, res, next) {
 router.put("/", async function(req, res, next) {
   // console.log(req);
   var copywriter = req.body.copywriter; // << params bo odbieram dane
+  
   if (!copywriter) {
-    return res.status(402).send("brak danych");
+    return res.status(400).send("brak danych");
   }
+  
   try {
     var result = await CopywritersInvoice.findByIdAndUpdate(copywriter._id, copywriter, doc => {
       return doc;
@@ -67,6 +87,7 @@ router.put("/", async function(req, res, next) {
   } catch (err) {
     res.status(400).json(err);
   }
+  
 });
 
 
@@ -75,9 +96,11 @@ router.put("/", async function(req, res, next) {
 router.delete("/:id", async function(req, res, next) {
   // console.log(req);
   var id = req.params.id; // <<<<<<<<<<< params bo odbieram dane
+  
   if (!id) {
-    return res.status(402).send("brak id");
+    return res.status(400).send("brak id");
   }
+  
   try {
     var result = await CopywritersInvoice.deleteOne({ _id: id }).exec();
     console.log(res.status(200).json(result));
