614283e1759d0aa1c487a83284ba856f5936d2b9
ghOSt1010 <43892935+ghOSt1010@users.noreply.github.com>
2020-03-29 18:35:28 +0200

    API implementation

---
+++ server/routes/API/API.js
@@ -0,0 +1,8 @@
+/*
+* TODO:
+*   - przesunąć routing logiki biznesowej do API
+*
+* wszystkie paczki API powrzucaj tutaj
+* dzieki temu bedziesz mial jedna nadrzedna sciezke API do zarządzenia
+* cos w rodzaju dodatkowego routera :) 
+*/
